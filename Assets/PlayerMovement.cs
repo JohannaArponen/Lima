54d741c6b9140b265803b80bdbc1f58af1ab028f
Johanna Arponen <johanna.arponen@icloud.com>
2020-05-11 13:57:49 +0300

    Kameran teko

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -17,12 +17,21 @@ public class PlayerMovement : MonoBehaviour{
         animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
         if (Input.GetButtonDown("Jump")) {
             jump = true;
+            animator.SetBool("IsJumping", true);
         }
         if (Input.GetButtonDown("Crouch")) {
             crouch = true;
         }else if (Input.GetButtonUp("Crouch")) {
             crouch = false;
         }
+    }
+
+    public void OnLanding() {
+        animator.SetBool("IsJumping", false);
+    }
+
+    public void OnCrouching(bool isCrouching) {
+        animator.SetBool("IsCrouching", isCrouching);
     }
 
     void FixedUpdate() {
