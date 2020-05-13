dc9f8519b838db3f35c524a0d2c6600febb9aeb9
Johanna Arponen <johanna.arponen@icloud.com>
2020-05-13 10:33:31 +0300

    Enemyn tekeminen

---
+++ Assets/Script/PatrollingEnemy.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PatrollingEnemy : MonoBehaviour{
+    public Transform[] waypoints;
+    public float waypointTriggerDistance = 1f;
+    int nextWaypoint = 0;
+    NavMeshAgent agent;
+
+    void Awake() {
+        agent = GetComponent<NavMeshAgent>();
+        agent.destination = waypoints[nextWaypoint].position;
+    }
+
+    void Update(){
+        var d = Vector3.Distance(transform.position, waypoints[nextWaypoint].position);
+        //(p2-p1).magnitude
+        if(d < waypointTriggerDistance) {
+            nextWaypoint++;
+            if(nextWaypoint > waypoints.Length - 1) {
+                nextWaypoint = 0;
+            }
+            agent.destination = waypoints[nextWaypoint].position;
+        }
+    }
+}
