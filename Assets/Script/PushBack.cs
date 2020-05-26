2c9b4e074d2caf36aa1eee181db29fd17b0815ae
Johanna Arponen <johanna.arponen@icloud.com>
2020-05-26 14:30:38 +0300

    Korjausta

---
+++ Assets/Script/PushBack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PushBack : MonoBehaviour{
+
+    public float push;
+    public Rigidbody2D rb;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+       /* d = "Player" - "Enemy";
+        d.y = 0;*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
